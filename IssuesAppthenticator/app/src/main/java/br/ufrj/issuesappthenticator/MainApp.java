45178eab74e3a18e2b7611edfb67574a25a64ee2
Victor Bustamante <victorbustamante12@gmail.com>
2017-12-07 22:53:32 -0200

    added new app compiled from last release

--- IssuesAppthenticator/app/src/main/java/br/ufrj/issuesappthenticator/MainApp.java
+++ IssuesAppthenticator/app/src/main/java/br/ufrj/issuesappthenticator/MainApp.java
@@ -11,33 +11,33 @@ public class MainApp extends Application
   @Override
   public void onCreate() {
     super.onCreate();
-    if (android.os.Build.VERSION.SDK_INT >= 26) {
 
-      NotificationManager mNotificationManager =
-          (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-      // The id of the channel.
-      String id = "my_channel_01";
-      // The user-visible name of the channel.
-      CharSequence name = "channel";
-      // The user-visible description of the channel.
-      String description = "channel_description";
-      int importance = NotificationManager.IMPORTANCE_HIGH;
-      NotificationChannel mChannel = null;
-
-      mChannel = new NotificationChannel(id, name, importance);
-      // Configure the notification channel.
-      mChannel.setDescription(description);
-      mChannel.enableLights(true);
-      // Sets the notification light color for notifications posted to this
-      // channel, if the device supports this feature.
-      mChannel.setLightColor(Color.RED);
-      mChannel.enableVibration(true);
-      mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
-      mNotificationManager.createNotificationChannel(mChannel);
-
-
-
-    }
-
+//    if (android.os.Build.VERSION.SDK_INT >= 26) {
+//
+//      NotificationManager mNotificationManager =
+//          (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//      // The id of the channel.
+//      String id = "my_channel_01";
+//      // The user-visible name of the channel.
+//      CharSequence name = "channel";
+//      // The user-visible description of the channel.
+//      String description = "channel_description";
+//      int importance = NotificationManager.IMPORTANCE_HIGH;
+//      NotificationChannel mChannel = null;
+//
+//      mChannel = new NotificationChannel(id, name, importance);
+//      // Configure the notification channel.
+//      mChannel.setDescription(description);
+//      mChannel.enableLights(true);
+//      // Sets the notification light color for notifications posted to this
+//      // channel, if the device supports this feature.
+//      mChannel.setLightColor(Color.RED);
+//      mChannel.enableVibration(true);
+//      mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+//      mNotificationManager.createNotificationChannel(mChannel);
+//
+//
+//
+//    }
   }
 }
